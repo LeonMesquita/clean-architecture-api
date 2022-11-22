ea35b7dbd8fc18ec7838555dc719b0c6bf085f72
LeonMesquita <leonmesquita16@gmail.com>
2022-11-22 14:41:32 -0300

    refactor: ensure SignUpController.handle implement correct interface

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,9 +3,11 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SingUpController()
     const httpRequest = {
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
@@ -14,9 +16,11 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = new SingUpController()
     const httpRequest = {
-      name: 'any_name',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
@@ -26,3 +30,5 @@ describe('SignUp Controller', () => {
 
 // Primeiro commitar sempre o arquivo de produção e depois o de teste
 // git add src/presentation/controllers/signup.ts
+// git commit -m "feat: ensure signUpController returns 400 if no email is provided"
+// git commit -m "test: ensure signUpController returns 400 if no email is provided"
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,20 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SingUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.name) {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
-    }
-    if (!httpRequest.email) {
+    } else if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
+      }
+    } else {
+      return {
+        statusCode: 200,
+        body: {}
       }
     }
   }
